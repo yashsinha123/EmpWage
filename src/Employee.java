4de185f3170de35fd5a7e356769cfe9b9a949082
yashsinha123 <ys9326@srmist.edu.in>
2024-01-22 16:03:06 +0530

    ispresnt

---
+++ src/Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+    String name;
+    Integer id;
+    Boolean attendance;
+
+     Employee( String n, Integer i){
+        this.name = n;
+        this.id = i;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee e1 = new Employee("yash", 1);
+
+        int ischeck = (int)((Math.random()*10))%2;
+        if (ischeck == 0) {
+            e1.attendance=false;
+
+        }
+        else{
+            e1.attendance=true;
+
+        }
+        if(e1.attendance){
+            System.out.println("Employee is present");
+        }
+        else{
+            System.out.println("Employee is not present");
+        }
+
     }
 }
