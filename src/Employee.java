7519a990e985e5a9b2bffb838b357054d2f110b9
yashsinha123 <ys9326@srmist.edu.in>
2024-01-22 16:29:47 +0530

    new feature: salary added

--- src/Employee.java
+++ src/Employee.java
@@ -2,10 +2,14 @@ public class Employee {
     String name;
     Integer id;
     Boolean attendance;
+    Integer wageperhour;
+    Integer workhour=0;
 
-     Employee( String n, Integer i){
+     Employee( String n, Integer i,Integer w){
+
         this.name = n;
         this.id = i;
+        this.wageperhour =w ;
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Employee e1 = new Employee("yash", 1);
+        Employee e1 = new Employee("yash", 1,20);
 
         int ischeck = (int)((Math.random()*10))%2;
         if (ischeck == 0) {
@@ -13,10 +13,18 @@ public class Main {
         }
         if(e1.attendance){
             System.out.println("Employee is present");
+            e1.workhour =8;
         }
         else{
             System.out.println("Employee is not present");
         }
+        //Main m1 = new Main();
+        System.out.println(calculatesalary(e1));
 
     }
+    static double  calculatesalary(Employee e){
+        return e.workhour*e.wageperhour;
+    }
+
+
 }
